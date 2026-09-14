2fb8d2e32a259393ce1263cf1e43419dc5742da9
Milleboy2007 <2484435@cegepmv.ca>
2026-09-14 14:56:36 +0000

    animation

--- projet/app/_layout.jsx
+++ projet/app/_layout.jsx
@@ -2,20 +2,22 @@ import { StyleSheet, Text, View, Platform } from 'react-native'
 import { Stack } from "expo-router"
 import "../global.css"
 import { StatusBar } from 'expo-status-bar';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
  
 const RootLayout = () => {
  
     return (
-        <>
-            <StatusBar style="light"  />
+        <GestureHandlerRootView>
+            <StatusBar style="dark"  />
             <Stack>
                 {/* <Stack.Screen name='index' options={{headerShown: false}}/> */}
                 {/* <Stack.Screen name='touchable' options={{headerShown: false}}/> */}
                 {/* <Stack.Screen name='Calculatrice' options={{headerShown: false}}/> */}
                 {/* <Stack.Screen name='app' options={{headerShown: false}}/> */}
-                <Stack.Screen name='poke' options={{headerShown: false}}/>
+                {/* <Stack.Screen name='poke' options={{headerShown: false}}/> */}
+                <Stack.Screen name='animation' options={{headerShown: false}}/>
             </Stack>
-        </>
+        </GestureHandlerRootView>
     )
 }
  
---
+++ projet/app/animation.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import {
+  Gesture, // Remplacement de useLongPressGesture
+  GestureDetector,
+  GestureHandlerRootView,
+} from 'react-native-gesture-handler';
+import { StyleSheet } from 'react-native';
+import Animated, {
+  Easing,
+  interpolateColor,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
+
+const COLORS = ['#b58df1', '#fa7f7c', '#ffe780', '#82cab2'];
+const easing = Easing.bezier(0.31, 0.04, 0.03, 1.04);
+
+export default function animation() {
+  const colorIndex = useSharedValue(0);
+  const nextColorIndex = useSharedValue(0);
+  const progress = useSharedValue(0);
+  const scale = useSharedValue(1);
+
+  // Utilisation de l'API Builder (Gesture.LongPress())
+  const longPress = Gesture.LongPress()
+    .onBegin(() => {
+      scale.value = withTiming(1.2, {
+        duration: 500,
+        easing: easing,
+      });
+    })
+    .onStart(() => { // onStart remplace onActivate
+      colorIndex.value = nextColorIndex.value;
+      nextColorIndex.value = (colorIndex.value + 1) % COLORS.length;
+      progress.value = 0;
+      progress.value = withTiming(1, {
+        duration: 500,
+        easing: easing,
+      });
+    })
+    .onFinalize(() => {
+      scale.value = withTiming(1, {
+        duration: 250,
+        easing: easing,
+      });
+    });
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    backgroundColor: interpolateColor(
+      progress.value,
+      [0, 1],
+      [COLORS[colorIndex.value], COLORS[nextColorIndex.value]]
+    ),
+    transform: [{ scale: scale.value }],
+  }));
+
+  return (
+    <GestureHandlerRootView style={styles.container}>
+      <GestureDetector gesture={longPress}>
+        <Animated.View style={[styles.box, animatedStyle]} />
+      </GestureDetector>
+    </GestureHandlerRootView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box: {
+    width: 100,
+    height: 100,
+    borderRadius: 20,
+    cursor: 'pointer',
+  },
+});
